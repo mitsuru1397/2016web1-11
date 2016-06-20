c578257229c0af2cd61f0eba843676684ea23e76
mitsuru1397 <g1527068@st.t-kougei.ac.jp>
2016-06-20 17:29:18 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,10 +2,10 @@ var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください!'));
 
 var message;
-if(answe === number) {
+if(answer === number) {
   message = 'あたり!';
 }
-else if (answe < number){
+else if (answer < number){
   message = '残念、それより大きい数字です';
 }
 else if(number < answe){
